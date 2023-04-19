f1240f1e85a4efb207137d980557c03d90f44482
Andrii Valenko <andreyvalenko@gmail.com>
2023-04-19 09:12:10 +0300

    s

--- src/js/router.js
+++ src/js/router.js
@@ -24,6 +24,7 @@ function router() {
 
     if (hashView?.title === 'details') {
       menuEl.classList.remove('is-open');
+
       app.insertAdjacentHTML('beforeend', hashView.render());
       return;
     }
