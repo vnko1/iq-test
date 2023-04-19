618cdf89695555b31d148dae319e150f23f3e49c
Andrii Valenko <andreyvalenko@gmail.com>
2023-04-19 15:25:44 +0300

    eds

--- src/js/router.js
+++ src/js/router.js
@@ -1,23 +1,26 @@
 import home from './views/home.js';
 import details from './views/details.js';
 import { headerTest } from './views/headerTest.js';
-import { page1 } from './views/tests.js';
+import { tests } from './views/tests.js';
 // import about from './views/about.js';
 // import contact from './views/contact.js';
 
 const menuEl = document.querySelector('.menu-container');
 const headerEl = document.getElementById('header-container');
+let testBtn = null;
 
 const routes = {
   '/': { title: 'Home', render: home },
+  '/test/1': { title: 'Test', render: tests },
+  '/test/2': { title: 'Test', render: tests },
   '#details': { title: 'details', render: details },
-  '/test/1': { title: 'Test', render: page1 },
   // '/contact': { title: 'Contact', render: contact },
 };
 
-async function router() {
+function router() {
   const view = routes[location.pathname];
   const hashView = routes[location.hash];
+
   if (location.pathname.includes('test')) {
     headerEl.innerHTML = headerTest();
   } else {
@@ -25,32 +28,22 @@ async function router() {
   }
 
   if (view || hashView) {
-    menuEl.classList.remove('is-open');
-    document.title = view.title;
-    app.innerHTML = view.render();
-
-    if (hashView?.title === 'details') {
-      menuEl.classList.remove('is-open');
-      await app.insertAdjacentHTML('beforeend', hashView.render());
-      const arrowEl = document.querySelector('.arrow-container');
-      const detailsEl = document.querySelector('.details-container');
-
-      setTimeout(() => arrowEl.classList.add('rotate'), 0);
-      setTimeout(() => detailsEl.classList.add('animate'), 0);
-    }
+    renderPage(view, location.pathname);
+    if (hashView) renderDetails(hashView);
   } else {
-    history.replaceState('', '', '/');
-    router();
+    renderHomePage();
   }
 }
 
 window.addEventListener('click', e => {
-  e.preventDefault();
   if (e.target.matches('[data-link]')) {
+    e.preventDefault();
+
     history.pushState('', '', e.target.href);
     router();
   }
   if (e.target.matches('[data-details-link]')) {
+    e.preventDefault();
     if (!location.hash) {
       history.pushState('', '', '#details');
     } else {
@@ -62,3 +55,46 @@ window.addEventListener('click', e => {
 
 window.addEventListener('popstate', router);
 window.addEventListener('DOMContentLoaded', router);
+
+async function renderDetails(view) {
+  menuEl.classList.remove('is-open');
+  await app.insertAdjacentHTML('beforeend', view.render());
+  const arrowEl = document.querySelector('.arrow-container');
+  const detailsEl = document.querySelector('.details-container');
+
+  setTimeout(() => arrowEl.classList.add('rotate'), 0);
+  setTimeout(() => detailsEl.classList.add('animate'), 0);
+}
+
+function renderPage(view, path) {
+  const page = parseInt(path[path.length - 1]) || 0;
+  menuEl.classList.remove('is-open');
+  document.title = view.title;
+  app.innerHTML = view.render(page);
+  const formEl = document.querySelector('.form');
+  testBtn = document.querySelector('.test-button');
+  if (formEl) {
+    formEl.addEventListener('change', onHandleChange);
+  }
+
+  if (testBtn) {
+    testBtn.addEventListener('click', onHandleClick);
+  }
+}
+
+function renderHomePage() {
+  history.replaceState('', '', '/');
+  router();
+}
+
+function onHandleChange(e) {
+  if (e.target.value !== '') {
+    testBtn.disabled = false;
+  }
+}
+
+function onHandleClick(e) {
+  const page = parseInt(location.pathname[location.pathname.length - 1]);
+  history.pushState('', '', `${location.origin}/test/${page + 1}`);
+  router();
+}
--- src/js/views/tests.js
+++ src/js/views/tests.js
@@ -1,1 +1,34 @@
-export const page1 = () => ``;
+export const tests = page => {
+  let width = 7 * page;
+  if (page === 14) width = 100;
+
+  if (page === 1) {
+    return `<section class="test">
+  <div class="container test-container">
+    <div class="progress-bar-container">
+      <div class="progress">
+        <div style="width: ${width}%" class="bar"></div>
+      </div>
+    </div>
+    <div class="form-container page1-form">
+      <form class="test-form form">
+        <p class="form-text">Ваш пол:</p>
+        <label class="form-label">
+          <input type="radio" name="sex" value="man" class="form-input" />
+          <span class="form-label-text">Мужчина</span>
+        </label>
+        <label class="form-label">
+          <input type="radio" name="sex" value="woman" class="form-input" />
+          <span class="form-label-text">Женщина</span>
+        </label>
+      </form>
+    </div>
+      <button type="click" class="test-button page1-btn" disabled>далее</button>
+  </div>
+</section>`;
+  }
+
+  if (page === 2) {
+    return `<div>page 2</div>`;
+  }
+};
