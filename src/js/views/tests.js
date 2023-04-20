cf7111ed346d62b9faa5f61bf3a1a1e9378e77a2
Andrii Valenko <andreyvalenko@gmail.com>
2023-04-20 19:43:23 +0300

    eds

--- src/js/views/tests.js
+++ src/js/views/tests.js
@@ -87,28 +87,28 @@ export default tests = page => {
   switch (page) {
     case 1:
       return page1(width);
-    // case 2:
-    //   return page2(width);
-    // case 3:
-    //   return page3(width);
-    // case 4:
-    //   return page4(width);
-    // case 5:
-    //   return page5(width);
-    // case 6:
-    //   return page6(width);
-    // case 7:
-    //   return page7(width);
-    // case 8:
-    //   return page8(width);
-    // case 9:
-    //   return page9(width);
-    // case 10:
-    //   return page10(width);
-    // case 11:
-    //   return page11(width);
-    // case 12:
-    //   return loader(width);
+    case 2:
+      return page2(width);
+    case 3:
+      return page3(width);
+    case 4:
+      return page4(width);
+    case 5:
+      return page5(width);
+    case 6:
+      return page6(width);
+    case 7:
+      return page7(width);
+    case 8:
+      return page8(width);
+    case 9:
+      return page9(width);
+    case 10:
+      return page10(width);
+    case 11:
+      return page11(width);
+    case 12:
+      return loader(width);
   }
 };
 
